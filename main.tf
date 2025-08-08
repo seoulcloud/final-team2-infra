7c563e7ac2d124d7c5166bd7dbc9668a729a35d2
cjsqudwns <cjsqudwns2781@gmail.com>
2025-08-08 14:03:47 +0900

    fix(monitoring): Grafana Loadbalancer DNS Output 수정

--- main.tf
+++ main.tf
@@ -396,16 +396,16 @@ resource "aws_route53_record" "redis_endpoint" {
 }
 
 # Grafana 외부 접속용 도메인(grafana.goteego.store)
-# resource "aws_route53_record" "grafana" {
-#   depends_on = [module.grafana]
+resource "aws_route53_record" "grafana" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "grafana.${var.domain_name}"
+  type    = "CNAME"
+  ttl     = 300
 
-#   zone_id = aws_route53_zone.main.zone_id
-#   name    = "grafana.${var.domain_name}"
-#   type    = "CNAME"
-#   ttl     = 300
+  records = [module.grafana.grafana_loadbalancer_dns]
 
-#   records = [module.grafana.loadbalancer_dns]
-# }
+  depends_on = [module.grafana]
+}
 
 # elasticache ==========================
 #test
--- modules/monitoring/grafana/outputs.tf
+++ modules/monitoring/grafana/outputs.tf
@@ -3,11 +3,11 @@ output "grafana_release_name" {
   value       = helm_release.grafana.name
 }
 
-# output "grafana_loadbalancer_dns" {
-#   description = "Grafana LoadBalancer DNS"
-#   value = try(
-#     data.kubernetes_service.grafana.status.load_balancer.ingress[0].hostname,
-#     data.kubernetes_service.grafana.status.load_balancer.ingress[0].ip,
-#     null
-#   )
-# }
+output "grafana_loadbalancer_dns" {
+  description = "Grafana LoadBalancer DNS"
+  value = try(
+    data.kubernetes_service.grafana.status[0]["load_balancer"]["ingress"][0]["hostname"],
+    data.kubernetes_service.grafana.status[0]["load_balancer"]["ingress"][0]["ip"],
+    null
+  )
+}
