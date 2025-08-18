d0a76db9d68112f754536c4683a2d620f0e21414
cjsqudwns <cjsqudwns2781@gmail.com>
2025-08-18 09:55:44 +0900

    feat(grafana): alb_security_group_id 동적 적용

--- modules/monitoring/grafana/main.tf
+++ modules/monitoring/grafana/main.tf
@@ -6,27 +6,37 @@ resource "helm_release" "grafana" {
   repository = "https://grafana.github.io/helm-charts"
   version    = var.chart_version
 
-  create_namespace = true
-
   # values.yaml 설정 적용
-  values = [file("${path.module}/values.yaml")]
+  # values.yaml 기본값 + 동적 override
+  values = [
+    file("${path.module}/values.yaml"),
+    yamlencode({
+      ingress = {
+        annotations = {
+          "alb.ingress.kubernetes.io/security-groups" = var.alb_security_group_id
+        }
+      }
+      datasources = {
+        "datasources.yaml" = {
+          apiVersion = 1
+          datasources = [
+            {
+              name      = "Prometheus"
+              type      = "prometheus"
+              url       = "http://kube-prometheus-stack-prometheus.monitoring.svc.cluster.local:9090"
+              access    = "proxy"
+              isDefault = true
+            }
+          ]
+        }
+      }
+    })
+  ]
 
   # admin 비밀번호는 민감하므로 별도 관리
   set_sensitive {
     name  = "adminPassword"
     value = var.grafana_admin_password
-  }
-
-  # Ingress annotation에 SG 동적 세팅
-  set {
-    # 점(.)은 키 경로 구분자라서 escape 필요
-    name  = "ingress.annotations.alb\\.ingress\\.kubernetes\\.io/security-groups"
-    value = var.alb_security_group_id
-  }
-  # Prometheus ClusterIP 동적 세팅
-  set {
-    name  = "datasources.datasources\\.yaml.datasources[0].url"
-    value = "http://kube-prometheus-stack-prometheus.monitoring.svc.cluster.local:9090"
   }
 
   depends_on = [var.depends_on_module]
@@ -41,6 +51,8 @@ resource "aws_security_group_rule" "alb_to_nodes_grafana" {
   source_security_group_id = var.alb_security_group_id
   security_group_id        = var.node_group_security_group_id
   description              = "Allow ALB to reach Grafana"
+  
+  depends_on = [ var.depends_on_module ]
 }
 
 # 1) 잠깐 대기해서 ALB 붙을 시간 주기
