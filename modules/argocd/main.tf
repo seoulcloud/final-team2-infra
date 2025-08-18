bc533faa2702e62dc38aa20b1d280e9056b3c722
seoulcloud <integrall92@gmail.com>
2025-08-19 08:56:09 +0900

    feat: argocd-values.yaml 파일 추가 및 ingress 설정에서 HTTPS 프로토콜로 변경

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -24,7 +24,7 @@ resource "helm_release" "argocd" {
             "alb.ingress.kubernetes.io/listen-ports"     = jsonencode([{ HTTP = 80 }, { HTTPS = 443 }])
             "alb.ingress.kubernetes.io/ssl-redirect"     = "443"
             "alb.ingress.kubernetes.io/certificate-arn"  = var.certificate_arn
-            "alb.ingress.kubernetes.io/backend-protocol" = "HTTP"
+            "alb.ingress.kubernetes.io/backend-protocol" = "HTTPS"
             "alb.ingress.kubernetes.io/healthcheck-path" = "/healthz"
             "alb.ingress.kubernetes.io/success-codes"    = "200-399"
             "alb.ingress.kubernetes.io/security-groups"  = var.alb_security_group_id
