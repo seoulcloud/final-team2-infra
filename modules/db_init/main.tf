1a629f174f72e9e486e0ab6f3a6fc4e569952f76
cjsqudwns <cjsqudwns2781@gmail.com>
2025-08-20 14:43:04 +0900

    fix(db_init): placeholder 문법을 Flyway에 맞게 수정

--- modules/db_init/main.tf
+++ modules/db_init/main.tf
@@ -6,13 +6,13 @@ locals {
     -- Exporter 유저 생성 & 권한 (비밀번호/유저명은 placeholder)
     DO $$
     BEGIN
-      IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '$${exporter_user}') THEN
-        EXECUTE format('CREATE ROLE %I LOGIN PASSWORD %L', '$${exporter_user}', '$${exporter_password}');
+      IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '${exporter_user}') THEN
+        EXECUTE format('CREATE ROLE %I LOGIN PASSWORD %L', '${exporter_user}', '${exporter_password}');
       END IF;
     END $$;
 
-    GRANT pg_monitor TO "$${exporter_user}";
-    GRANT CONNECT ON DATABASE "$${db_name}" TO "$${exporter_user}";
+    GRANT pg_monitor TO "${exporter_user}";
+    GRANT CONNECT ON DATABASE "${db_name}" TO "${exporter_user}";
     SQL
   )
 
